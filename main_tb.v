b4429057c7d6aaf6a4011eca9757b79e06def29b
viasana <107901948+viaasana@users.noreply.github.com>
2025-07-05 22:27:44 +0700

    Testbench mới cho mạch có thêm tính hiệu start

--- main_tb.v
+++ main_tb.v
@@ -5,12 +5,14 @@ module sum1to100_tb ;
 
     reg clk = 0;
     reg rst = 0;
-    wire unsigned [15:0] result;
+    reg start = 0;
+    wire unsigned [12:0] result;
     wire finish;
 
     sum1to100 uut (
         .clk(clk),
         .rst(rst),
+        .start(start),
         .result(result),
         .finish(finish)
     );
@@ -20,6 +22,14 @@ module sum1to100_tb ;
     initial begin
         #10;
         rst = 1;
+        #10;
+        start = 1;
+        #1100;
+        rst=0;
+        start = 0;
+        #10;
+        rst = 1;
+        start = 1;
     end
 
 endmodule
